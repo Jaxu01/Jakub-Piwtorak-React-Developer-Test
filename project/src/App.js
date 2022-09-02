3b6db168729bf115aa0fdfb4d47237381a985bc7
JakubP <jakubpiwtorak@gmail.com>
2022-09-02 20:32:37 +0200

    First entry

---
+++ project/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="desktop">
+        <div className="navigation">
+          <ul>
+            <li>WOMEN</li>
+            <li>MEN</li>
+            <li>KIDS</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
