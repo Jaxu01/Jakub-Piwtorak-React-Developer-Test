c0bd71ea1d9105226bb7e2fb4eb9cf91ec79aee1
JakubP <jakubpiwtorak@gmail.com>
2022-09-30 22:15:36 +0200

    Changes

--- src/Currencies.js
+++ src/Currencies.js
@@ -4,42 +4,52 @@ import { Query, client } from '@tilework/opus';
 
 function Currencies() {
 const [currencies, setCurrencies] = useState([]);
-const [activeCurrency, setActiveCurrency] = useState([]);
+const [activeCurrency, setActiveCurrency] = useState({symbol : "$"});
 const [dropdownOpen, setDropdownOpen] = useState(false);
 
     useEffect(() => {
         const fetchData = (async() => {
-        client.setEndpoint('http://localhost:4000/')
-        const query = new Query('currencies', true)
+            client.setEndpoint('http://localhost:4000/')
+            const query = new Query('currencies', true)
             .addFieldList(['label', 'symbol']);
-        
-        const result = await client.post(query);
-        setCurrencies(result.currencies)
-        console.log(result)
+            
+            const result = await client.post(query);
+            setCurrencies(result.currencies)
+            console.log(result)
         });
         fetchData()
+        setDropdownOpen(false)
     }, []);
 
     return (
         <>
-            <div className="section-right">
-                {dropdownOpen && <Dropdown currencies={currencies} />}
+            <div className="default-currency">
+                {activeCurrency && <Currency activeCurrency={activeCurrency} />}
             </div>
-            <svg onClick={() => setDropdownOpen((prev) => !prev)}  width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div className="section-right-currencies">
+                {dropdownOpen && <Dropdown currencies={currencies} setActiveCurrency={setActiveCurrency} />}
+            </div>
+            <svg onClick={() => setDropdownOpen((prev) => !prev)} width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1 0.5L4 3.5L7 0.5" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
         </>
     )
 }
 
-function Dropdown({currencies}) {
+function Currency({activeCurrency}) {
+    return (
+        <div className="active-currency">
+            <p>{activeCurrency.symbol}</p>
+        </div>
+    )
+}
+
+function Dropdown({setActiveCurrency, currencies}) {
     return (
         <div className="currencies-container">
             {currencies?.map((currency, index) => (
-                <div key={index} className="currency-div">
-                    <div className="symbol"></div>
+                <div key={index} onClick={() => setActiveCurrency(currency)} className="currency-div">
                     <p>{currency.symbol}</p>
-                    <div className="label"></div>
                     <p>{currency.label}</p>
                 </div>
             ))}
