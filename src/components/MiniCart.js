105f8b47d92c6823b94ac724e7983dbbe9d01899
JakubP <jakubpiwtorak@gmail.com>
2023-02-15 20:21:31 +0100

    Changes

--- src/components/MiniCart.js
+++ src/components/MiniCart.js
@@ -8,12 +8,11 @@ const MiniCart = ({currency}) => {
     const [cartProducts, setCartProducts] = useState([]);
     const minicartStorage = localStorage.getItem("minicart")
     const minicart = JSON.parse(minicartStorage)
-    console.log(minicartStorage)
     console.log(cartProducts)
     
-    const fetchProduct = async(productId) => {
+    const fetchProduct = async(cartItem) => {
         const query = new Query('product', true)
-        .addArgument('id', 'String!', productId)
+        .addArgument('id', 'String!', cartItem.productId)
         .addFieldList(['name', 'gallery', 'description', 'brand'])
         .addField(new Field('prices')
             .addFieldList(['amount'])
@@ -29,14 +28,14 @@ const MiniCart = ({currency}) => {
         )
         const {product} = await client.post(query)
         const activePrice = new Object(product.prices.find(price => price.currency.label === currency.label))
-        return {...Object(product), activePrice}
+        return {...Object(product), activePrice, choices: cartItem}
     }
 
     const fetchData = () => {
         const products = []
         if (minicart.length) {
             minicart.forEach(async cartItem => {
-                products.push(await fetchProduct(cartItem.productId))
+                products.push(await fetchProduct(cartItem))
             })
         }
         setCartProducts(products)
@@ -48,31 +47,41 @@ const MiniCart = ({currency}) => {
                 })()
     }, [])
 
+    
     return (
         <Dropdown title={<CartIcon></CartIcon>}> 
-            {cartProducts.length && cartProducts.map(cartProduct => (
-                <>
-                    <div>{cartProduct.name}</div>
-                    <div>{cartProduct.brand}</div>
-                    <div>{cartProduct.activePrice.currency.symbol}{cartProduct.activePrice.amount}</div>
-                    <div>{cartProduct.attributes.map(function (attribute, index) {
-                                return (
-                                    <div key={index}>{attribute.name}
-                                        {attribute.items.map(function (item, index) {
-                                            return (
-                                                    <label key={index} htmlFor={item.id}> 
-                                                        <input value={item.value} id={item.id} name={attribute.name} type="radio"/>
-                                                            <div className="radio-tile">
-                                                                {item.displayValue}
-                                                            </div>
-                                                    </label>
-                                            )
-                                        })}
-                                    </div>
-                                )
-                            })}</div>
-                    <img src={cartProduct.gallery[0]}/>
-                </>
+                {!cartProducts.length &&
+                    (
+                        <p>No Items Available</p>
+                    )
+                }
+            {!!cartProducts.length && cartProducts.map((cartProduct, index) => (
+                    <div key={index} className="product-info">
+                        <div className="information-box">
+                            <div>{cartProduct.name}</div>
+                            <div>{cartProduct.brand}</div>
+                            <div className="price">{cartProduct.activePrice.currency.symbol}{cartProduct.activePrice.amount}</div>
+                            <div>{cartProduct.attributes.map(function (attribute, index) {
+                                        return (
+                                            <div className="minicart-radio-tiles" style={{backgroundColor: cartProduct.choices[attribute.id]}} key={index}>
+                                                {attribute.name}
+                                                {attribute.items.map(function (item, index) {
+                                                    return (
+                                                            <label key={index} htmlFor={item.id} style={{backgroundColor: item.value}}> 
+                                                                    <div className="minicart-radio-tile">
+                                                                        {item.displayValue}
+                                                                    </div>
+                                                            </label>
+                                                    )
+                                                })}
+                                            </div>
+                                        )
+                                })}</div>
+                            </div>
+                        <div className="image-box">
+                            <img src={cartProduct.gallery[0]}/>
+                        </div>
+                    </div>
             ))}
         </Dropdown>
     )
