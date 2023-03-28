86d98f46a440aa11346e69d0abdefd8d34a92273
Jakub Piwtorak <jakubpiwtorak@gmail.com>
2023-03-28 20:38:54 +0200

    Changes

--- src/components/GallerySwitcher.js
+++ src/components/GallerySwitcher.js
@@ -1,23 +1,27 @@
-import { useState } from "react"
+import { Component } from "react"
 import "./GallerySwitcher.css"
 
-const GallerySwitcher = ({gallery}) => {
-    const [currentIndex, setCurrentIndex] = useState(0)
-    console.log(currentIndex)
-
-    const isFirstImage = currentIndex === 0
-    const isLastImage = currentIndex === gallery.length - 1
-    return (
-        <>
-            <div className="picture-switches">
-                <p className={`left-switch ${isFirstImage && "disabled"}`} onClick={() => setCurrentIndex(prev => prev - 1)}></p>
-                <p className={`right-switch ${isLastImage && "disabled"}`} onClick={() => setCurrentIndex(prev => prev + 1)}></p>
-            </div>
-            <div className="featured-picture">
-                <img src={gallery[currentIndex]}/>
-            </div>
-        </>
-    )
+export class GallerySwitcher extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {galleryIndex: 0}
+    }
+    
+    render () {
+        this.isFirstImage = this.state.galleryIndex === 0
+        this.isLastImage = this.state.galleryIndex === this.props.gallery.length - 1
+        return (
+            <>
+                <div className="picture-switches">
+                    <p className={`left-switch ${this.isFirstImage && "disabled"}`} onClick={() => this.setState(state => ({galleryIndex: state.galleryIndex - 1}))}></p>
+                    <p className={`right-switch ${this.isLastImage && "disabled"}`} onClick={() => this.setState(state => ({galleryIndex: state.galleryIndex + 1}))}></p>
+                </div>
+                <div className="featured-picture">
+                    <img src={this.props.gallery[this.state.galleryIndex]}/>
+                </div>
+            </>
+        )
+    }
 }
 
 export default GallerySwitcher
