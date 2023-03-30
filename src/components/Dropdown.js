06bd62bc59f1765492d52c935e4b895971f139db
Jakub Piwtorak <jakubpiwtorak@gmail.com>
2023-03-30 18:46:38 +0200

    Changes

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,26 +1,34 @@
-import { useState, useEffect } from "react"
+import { Component } from "react"
 import './Dropdown.css'
 
-const Dropdown = ({children, title, dispatchEvent}) => {
-    const [dropdownOpen, setDropdownOpen] = useState(false)
+export class Dropdown extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { dropdownOpen: false }
+  }
 
-     useEffect(() => {
-        dispatchEvent && document.addEventListener(dispatchEvent, ({detail}) => {
-            setDropdownOpen(detail.open)
-        })
-    }, [])
+  componentDidMount() {
+    const { dispatchEvent } = this.props
+    dispatchEvent &&
+      document.addEventListener(dispatchEvent, ({ detail }) => {
+        this.setState({ dropdownOpen: detail.open })
+      })
+  }
 
+  render() {
+    const { children, title } = this.props
+    const { dropdownOpen } = this.state
     return (
-        <div className="dropdown">
-            <div className="dropdown-title" onClick={() => setDropdownOpen(prev => !prev)}>{title}</div>
-            
-            {dropdownOpen && (
-                <>
-                    <div className="dropdown-content">{children}</div>
-                </>        
-            )}
+      <div className="dropdown">
+        <div className="dropdown-title" onClick={() => this.setState((state) => ({ dropdownOpen: !state.dropdownOpen }))}>
+          {title}
         </div>
+        {dropdownOpen && (
+            <div className="dropdown-content">{children}</div>
+        )}
+      </div>
     )
+  }
 }
 
 export default Dropdown
